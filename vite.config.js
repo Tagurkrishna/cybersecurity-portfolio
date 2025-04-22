df2a333a5ce345d09aaf20fbc9d5a6b32e8d6ce2
Tagur Krishna Nethipudi <48682753+Tagurkrishna@users.noreply.github.com>
2025-04-22 13:22:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // <-- add this
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // <-- add this
+    },
+  },
 })
